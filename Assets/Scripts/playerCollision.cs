4f1fe55e103af37783afd059a6c5d999282e5e3c
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-06-23 17:13:01 +0200

    playerCollision and playerMovement refactor + documentation

--- Assets/Scripts/playerCollision.cs
+++ Assets/Scripts/playerCollision.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 
 public class playerCollision : MonoBehaviour {
-	
+
+	/*
+	This class is responsible for handling the collision.
+	OnCollisionEnter goes after FixedUpdate on unity execution order so maybe
+	some collisions will be undetected. To fix this we can try to build our
+	collision system but for now will work with that way
+	*/
+
 	public Rigidbody rb;
 	public playerMovement playerMovement;
-	//This function will be called every time an object collides with another
+	/*
+	Pre: A collision has ocurred
+	Post: If the collision has happened with an obstacle, this fuction applys a force against the player movement
+	to create a better sense of impact
+	*/
 	void OnCollisionEnter(Collision collisionInformation) {
 		//Debug.Log("We hit " + collisionInformation.collider.name);
 		if(collisionInformation.collider.tag == "Obstacle") {
 			Vector3 offset = new Vector3(0f, 0f, -500f);
 			rb.AddForce(offset);
-			//disable movement
-			playerMovement.enabled = false;
+			playerMovement.enabled = false; //disable movement
 			gameManager gameManager = FindObjectOfType<gameManager>();
 			gameManager.endGame();
 		}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -3,26 +3,18 @@
 //extends MonoBehaivour
 public class playerMovement : MonoBehaviour {
 
+/*
+This class handles player movement
+*/
 	public Rigidbody rb;
 	public int forwardForce = 7000;
 	public int middle = Screen.width/2;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void FixedUpdate () {
 		rb.transform.rotation = Quaternion.Euler(0, 0, 0);
 		rb.AddForce(0, 0, 750*Time.deltaTime);
-		//Debug.Log(rb.position.z);
-		if(rb.position.z > /*125f*/ 100000000f & rb.position.y >= 0) {
-			this.enabled = false; //the end of the level
-			rb.AddForce(0, 0, -(forwardForce*2)*Time.deltaTime, ForceMode.VelocityChange);
-		}
-		else if(rb.position.y < -1f) {
-			FindObjectOfType<gameManager>().endGame();
+		if(rb.position.y < -1f) {
+			FindObjectOfType<gameManager>().endGame(); //we are falling of the road
 		}
 		else {
 			//for mobile devices:
