0b67b63387640b12f28db93691ba31904f04182f
roguib <roger.guasch.ibarra@est.fib.upc.edu>
2019-06-24 00:26:49 +0200

    added a play/pause button

---
+++ Assets/Scripts/GUIcontroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIcontroller : MonoBehaviour {
+
+	/*
+	This class handles all GUI elements
+	*/
+
+	public Button pauseButton;
+	private bool gameIsBeingPlayed = true;
+	void Start () {
+		//ini listeners
+		pauseButton.onClick.AddListener(pauseGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void pauseGame() {
+		if(gameIsBeingPlayed) {
+			gameIsBeingPlayed = false;
+			Time.timeScale = 0;
+			pauseButton.GetComponentInChildren<Text>().text = "Play";
+		}
+		else {
+			gameIsBeingPlayed = true;
+			Time.timeScale = 1;
+			pauseButton.GetComponentInChildren<Text>().text = "Pause";
+		}
+	}
+}
